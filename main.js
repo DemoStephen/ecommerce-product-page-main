671ba79bb079748458d18df325512c1baf637e66
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-09 23:47:53 +0100

    Added comments to further explain my code

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ const incrementBtn = document.querySelector('[data-incrementBtn]');
 let actualValue = +quantity.innerText;
 price = document.querySelector('[data-price]');
 discount = document.querySelector('[data-discount]');
-
+// This block of code contain an event listener that shows the sideBar on a mobile screen
 openSideBar.addEventListener('click', (event)=>{
     event.preventDefault();
     sideBar.classList.remove('d-none')
 })
+// This block of code contain an event listener that hides the sideBar on a mobile screen
 closeSideBar.addEventListener('click', (event)=>{
     event.preventDefault();
     if(sideBar.classList.contains('d-block')){
@@ -19,7 +20,7 @@ closeSideBar.addEventListener('click', (event)=>{
     }
     sideBar.classList.add('d-none')
 })
-
+// This block of code contain an event listener that decreases the amount of product users want
 decrementBtn.addEventListener('click', (event)=>{
     event.preventDefault();
     if(actualValue === 0){
@@ -29,6 +30,7 @@ decrementBtn.addEventListener('click', (event)=>{
     }priceCalculator()
     return quantity.innerText = actualValue
 })
+// This block of code contain an event listener that increases the amount of product users want
 incrementBtn.addEventListener('click', (event)=>{
     event.preventDefault();
     actualValue +=1
@@ -36,6 +38,7 @@ incrementBtn.addEventListener('click', (event)=>{
     return quantity.innerText = actualValue
 })
 
+// This a function that calculate the price of the product related to the quantity selected by users
 const priceCalculator = ()=>{
     if(actualValue === 0){
         price.innerText = '$125.00'
@@ -45,7 +48,7 @@ const priceCalculator = ()=>{
         discount.innerText = `$${actualValue * 250}.00`
     }
 }
-
+// This is my photo gallery section. I need help here. Its take time for my image to load.
 const imageThumbnail = document.querySelectorAll('[data-imageThumbnail]');
 const banner = document.querySelector('[data-banner]');
 const bannerGallery = [
