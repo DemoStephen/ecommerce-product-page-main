9d88dde47c019044b016a178e89b89d3d6c715a1
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-08-09 19:54:41 +0100

    Updated the main code

--- main.js
+++ main.js
@@ -1,40 +1,68 @@
-const decreaseCount = document.querySelector('[data-decreaseCount]');
-const count = document.querySelector('[data-count]');
-const increaseCount = document.querySelector('[data-increaseCount]');
-const price = document.querySelector('[data-price]');
-const fixedPrice = document.querySelector('[data-fixedPrice]')
-let counter = +count.innerText;
-function plusAndMinus(){
-    let calculatePrice = ()=>{
-        if(counter === 0){
-            return
-        }
-        let pricing = 125*counter;
-        let fixedpricing = 250*counter;
-        price.innerText = `$${pricing}.00`
-        fixedPrice.innerText = `$${fixedpricing}.00`
-    }
-    decreaseCount.addEventListener('click', (event)=>{
-        event.preventDefault()
-        if(counter === 0){
-            return counter = 1;
-        }
-        counter -=1
-        calculatePrice()
-        return count.innerText = counter;
-    })
-    increaseCount.addEventListener('click', (event)=>{
-        event.preventDefault()
-        counter +=1
-        calculatePrice()        
-        return count.innerText = counter;
-    })
-}
-plusAndMinus()
+const sideBar = document.querySelector('[data-sideBar]');
+const closeSideBar = document.querySelector('[data-closeSideBar]');
+const openSideBar = document.querySelector('[data-openSideBar]');
+const decrementBtn = document.querySelector('[data-decrementBtn]');
+const quantity = document.querySelector('[data-quantity]');
+const incrementBtn = document.querySelector('[data-incrementBtn]');
+let actualValue = +quantity.innerText;
+price = document.querySelector('[data-price]');
+discount = document.querySelector('[data-discount]');
 
-const imageThumbnails = document.querySelectorAll('[data-imageThumbnail]');
-imageThumbnails.forEach((imageThumbnail)=>{
-    imageThumbnail.addEventListener('click', ()=>{
-        imageThumbnail.style.border = '2px solid var(--Orange)'
-    })
+openSideBar.addEventListener('click', (event)=>{
+    event.preventDefault();
+    sideBar.classList.remove('d-none')
+})
+closeSideBar.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if(sideBar.classList.contains('d-block')){
+        sideBar.classList.remove('d-block')
+    }
+    sideBar.classList.add('d-none')
+})
+
+decrementBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if(actualValue === 0){
+        actualValue = 0;
+    }else{
+        actualValue -=1
+    }priceCalculator()
+    return quantity.innerText = actualValue
+})
+incrementBtn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    actualValue +=1
+    priceCalculator()
+    return quantity.innerText = actualValue
+})
+
+const priceCalculator = ()=>{
+    if(actualValue === 0){
+        price.innerText = '$125.00'
+        discount.innerText = '$250.00'
+    }else{
+        price.innerText = `$${actualValue * 125}.00`
+        discount.innerText = `$${actualValue * 250}.00`
+    }
+}
+
+const imageThumbnail = document.querySelectorAll('[data-imageThumbnail]');
+const banner = document.querySelector('[data-banner]');
+const bannerGallery = [
+    {src:'./images/image-product-1.jpg'},
+    {src:'./images/image-product-2.jpg'},
+    {src:'./images/image-product-3.jpg'},
+    {src:'./images/image-product-4.jpg'}
+]
+bannerGallery.forEach((gallery) => {
+    console.log(gallery)
+});
+
+imageThumbnail.forEach((thumbnail) => {
+    
+    thumbnail.addEventListener('click', (event)=>{
+        event.preventDefault();
+
+        banner.innerHTML = `<img data-imageThumbnail="imageThumbnail" src="${bannerGallery[i].src}" alt="image product" class="width borderRadius">`;
+    })  
 })
