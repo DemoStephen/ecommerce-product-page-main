b13d3d5328685e23da6302c7dd1c6f689b34052f
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-26 00:45:37 +0100

    Core Importance updated

---
+++ shoe-ecommerce/src/components/Core.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line react/prop-types
+export default function Core({ title }) {
+  return (
+    <div className="w-full md:pr-8 md:text-left text-center">
+      <h3 className="my-4 text-2xl">{title}</h3>
+      <p className="">
+        We deliver to your door step across all west african countries
+      </p>
+    </div>
+  );
+}
---
+++ shoe-ecommerce/src/components/CoreImportance.jsx
@@ -0,0 +1,11 @@
+import Core from "./Core";
+
+export default function CoreImportance() {
+  return (
+    <section className="px-[5%] flex justify-between gap-4 md:flex-row flex-col">
+      <Core title="Fast Shipping" />
+      <Core title="Free Click & Collect" />
+      <Core title="Save & Shop Safely" />
+    </section>
+  );
+}
