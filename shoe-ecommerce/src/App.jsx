aac5bad68c5792d312eb96b384413b8134dbde16
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-25 04:14:41 +0100

    created and modified app.jsx file

---
+++ shoe-ecommerce/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import SpiceUp from "./components/SpiceUp";
+import TrendingNow from "./components/TrendingNow";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Header />
+      <TrendingNow />
+      <SpiceUp />
+    </>
+  );
+}
+
+export default App;
