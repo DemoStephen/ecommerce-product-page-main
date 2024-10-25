c4f52522658783900b68870f946458aa5fde8ad6
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-26 00:49:20 +0100

    header component updated

--- shoe-ecommerce/src/App.jsx
+++ shoe-ecommerce/src/App.jsx
@@ -1,5 +1,9 @@
+import CoreImportance from "./components/CoreImportance";
+import FeaturedProduct from "./components/FeaturedProduct";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
+import SpecialOffer from "./components/SpecialOffer";
 import SpiceUp from "./components/SpiceUp";
 import TrendingNow from "./components/TrendingNow";
 
@@ -10,6 +14,11 @@ function App() {
       <Header />
       <TrendingNow />
       <SpiceUp />
+      <FeaturedProduct />
+      <hr className="my-24 border w-[90%] ml-[5%] md:block hidden" />
+      <CoreImportance />
+      <SpecialOffer />
+      <Footer />
     </>
   );
 }
