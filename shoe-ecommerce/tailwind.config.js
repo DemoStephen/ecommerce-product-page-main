e7c665cc18ea947269b7577daa9fdded937947f5
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-25 04:25:15 +0100

    setting up postcss with tailwind

---
+++ shoe-ecommerce/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "header-hero":
+          "url('https://images.unsplash.com/photo-1485736231968-0c8ad5c9e174?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      },
+    },
+  },
+  plugins: [],
+};
