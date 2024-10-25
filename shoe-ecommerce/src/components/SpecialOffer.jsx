4c53c623c91258135b653b9b171d8e6df8efcd3f
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-26 00:48:45 +0100

    special offer created

---
+++ shoe-ecommerce/src/components/SpecialOffer.jsx
@@ -0,0 +1,12 @@
+export default function SpecialOffer() {
+  return (
+    <>
+      <div className="grid md:grid-cols-3 md:grid-rows-2 p-[5%] md:gap-6 gap-4 h-screen">
+        <div className="bg-slate-300 md:row-span-2"></div>
+        <div className="bg-slate-300"></div>
+        <div className="bg-slate-300 md:row-span-2"></div>
+        <div className="bg-slate-300"></div>
+      </div>
+    </>
+  );
+}
